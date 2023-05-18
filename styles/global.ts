5d2c3d9c3d800c9ae38a72ffd2e211ceb47548d0
김솔지 <so1gging@gmail.com>
2023-05-18 15:47:21 +0900

    global style 적용 / 폰트 적용

---
+++ styles/global.ts
@@ -0,0 +1,23 @@
+import { css } from '@emotion/react'
+
+const global = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
+
+export default global
