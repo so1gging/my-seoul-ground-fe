32edaa2fc818bd04099987afc270bafb90aaf111
김솔지 <so1gging@gmail.com>
2023-05-18 15:56:31 +0900

    주석 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import global from '@/styles/global'
 import { Roboto } from 'next/font/google'
 
 const roboto = Roboto({
-  subsets: ['latin'], // preload에 사용할 subsets입니다.
+  subsets: ['latin'],
   weight: ['100', '400', '700'],
-  variable: '--roboto', // CSS 변수 방식으로 스타일을 지정할 경우에 사용합니다.
+  variable: '--roboto',
 })
 
 export default function App({ Component, pageProps }: AppProps) {
